4f74b7f9c4b2e3c4d3911af1a03a3588b08fb7a3
Nicolas <chincholax@gmail.com>
2018-11-20 14:52:44 -0400

    modificaciones incompletas

--- ValidadorIP/validador4.c
+++ ValidadorIP/validador4.c
@@ -13,21 +13,40 @@ int esNumero(char numero[]);
 
 int main(void) {
 
-	char ip_entrada[15];
+	char ip_entrada[16];
 	fprintf(stderr, "ingresar una ip: \n");
 	scanf("%s", ip_entrada);
 
 	unsigned int ip = esNumero(ip_entrada);
 
-
+	fprintf(stderr, "longitud de IP : %d\n", strlen(ip_entrada));
 
 	fprintf(stderr, "esta es la ip ingresada (%s) y esta es la transformada (%d) \n", ip_entrada, ip);
 
    return 0;
 }
 
-unsigned int Readdir(char direccion[]){
+unsigned int readDir(char direccion[]){
 
+	unsigned int aux[4];
+	char dir[16] = "";
+
+	if(strlen(direccion) <= 16){
+		
+		for(int i = 0; i < 16; i++){
+			dir[i] = direccion[i];
+		}
+
+		for(int i = 0; i < 4; i++){
+			aux[i] = esNumero(direccion);
+			dir = strchr(dir, '.');
+		}
+	} else {
+		error("La direccion escrita es más larga de lo que debería");
+		return FALSE;
+	}
+
+	return aux;
 }
 
 int esNumero(char numero[]){
@@ -43,7 +62,7 @@ int esNumero(char numero[]){
 	if(verificado){
 		return n;
 	} else {
-		return ERROR;
+		error("Los datos ingresados deben ser una combinación de números y puntos");
 	}
 
 }
