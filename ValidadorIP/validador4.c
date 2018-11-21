20d1cd4a0b0f37bda5ad9e42ae278d2d1abbb1b5
Nicolas <chincholax@gmail.com>
2018-11-20 21:31:22 -0400

    verificar el tipo de direccionamiento y salidas de error en sus respectivos casos

--- ValidadorIP/validador4.c
+++ ValidadorIP/validador4.c
@@ -11,8 +11,9 @@
 unsigned int readDir(char direccion[]);
 unsigned int verifMask(int mascara);
 int contarPuntos(char direccion[]);
-void ipClass(unsigned int ip);
-void gatewayValido(unsigned int ip, unsigned int mascara,unsigned int gateway);
+int ipClass(unsigned int ip);
+int verifClass(unsigned int direccion, int mascara);
+int gatewayValido(unsigned int ip, unsigned int mascara,unsigned int gateway);
 
 int main(void) {
 
@@ -22,10 +23,10 @@ int main(void) {
 	
 	printf("ingresar una ip: \n");
 	scanf("%s", ip_entrada);
-	printf("ingresar la máscara de subred en notación \"/24\"\n");
+	printf("ingresar la cantidad de unos de la máscara de subred\n");
 	scanf("%d", &mascara);
 	
-	printf("ingresar el gateway: \n");
+	printf("ingresar la direccion gateway: \n");
 	scanf("%s", gateway_entrada);
 
 	unsigned int ip = readDir(ip_entrada);
@@ -36,6 +37,7 @@ int main(void) {
 
 	fprintf(stderr, "esta es la ip ingresada (%s) y esta es la transformada (%u) \n", ip_entrada, ip);
 	fprintf(stderr, "verifMask: (%u)\n", mask);
+	verifClass(ip, mascara);
 
    return 0;
 }
@@ -44,17 +46,19 @@ unsigned int readDir(char direccion[]){
 
 	unsigned int aux1 = 0, aux2 = 0, aux3 = 0, aux4 = 0;
 
-	fprintf(stderr, "longitud de IP : %ld y la validacion: %ld\n", strlen(direccion), strspn(direccion, "0123456789."));
+	//fprintf(stderr, "longitud de IP : %ld y la validacion: %ld\n", strlen(direccion), strspn(direccion, "0123456789."));
 
 	if(strlen(direccion) <= 16 && strspn(direccion, "0123456789.") == strlen(direccion) && contarPuntos(direccion)){
 		sscanf(direccion, "%u.%u.%u.%u", &aux1, &aux2, &aux3, &aux4);
 	} else {
 		perror("La direccion escrita es más larga de lo que debería, incluye caracteres no numéricos o está incompleta");
+		exit(0);
 		return FALSE;
 	}
 
 	if((aux1|aux2|aux3|aux4) > 255){
 		perror("los valores están fuera de rango");
+		exit(0);
 	}
 
 	fprintf(stderr, "la direccion verificada: %u.%u.%u.%u\n", aux1, aux2, aux3, aux4);
@@ -83,6 +87,7 @@ unsigned int verifMask(int mascara){
 		}
 	} else {
 		perror("La máscara de subred debe estar entre 1 y 32");
+		exit(0);
 	}
 
 	return aux;
@@ -103,31 +108,60 @@ int contarPuntos(char in[]){
    }
 }
 
-void ipClass(unsigned int ip)
+int ipClass(unsigned int ip)
 {
     if ((ip & 0x80000000)==0){
-        puts("Clase A");
+        printf("Clase A\n");
     } else if((ip & 0xC0000000)==0x80000000)
     {
-        puts("Clase B");
+        printf("Clase B\n");
     }else if((ip & 0xE0000000)==0xC0000000)
     {
-        puts("Clase C");
-    }else if((ip & 0xE1000000)==0xE0000000)
+        printf("Clase C\n");
+    }else if((ip & 0xF0000000)==0xE0000000)
     {
-        puts("Clase D");
+        printf("Clase D\n");
     }
     else{
-        puts("Clase E");
+        printf("Multicast\n");
     }
+    return TRUE;
 }
-void gatewayValido(unsigned int ip,unsigned int mascara,unsigned int gateway)
+
+int verifClass(unsigned int ip, int mascara){
+	unsigned int ip4,ip3,ip2,ip1;
+	ip4 = ip >> 24;
+	ip3 = (ip >> 16)&0xff;
+	ip2 = (ip >> 8)&0xff;
+	ip1 = ip&0xff;
+	if(ip4 <= 127 && mascara < 8){
+		printf("El direccionamiento es CIDR\n");
+	} else if(ip4 <= 191 && ip4 >= 128 && mascara < 16){
+		printf("El direccionamiento es CIDR\n");
+	} else if(ip4 <= 223 && ip4 >= 192 && mascara < 24){
+		printf("El direccionamiento es CIDR\n");
+	} else if(ip4 <= 127 && mascara > 8){
+		printf("El direccionamiento es SubNetting\n");
+	} else if(ip4 <= 191 && ip4 >= 128 && mascara > 16){
+		printf("El direccionamiento es SubNetting\n");
+	} else if(ip4 <= 223 && ip4 >= 192 && mascara > 24){
+		printf("El direccionamiento es SubNetting\n");
+	} else{
+		ipClass(ip);
+	}
+	fprintf(stderr, "verifClass: %u.%u.%u.%u/%d\n", ip4, ip3, ip2, ip1, mascara);
+	return TRUE;
+}
+
+int gatewayValido(unsigned int ip,unsigned int mascara,unsigned int gateway)
 {
     //fprintf(stderr, "ip: %u y gw: %u", ip&mascara, gateway);
     if ((ip&mascara) == (gateway&mascara)){
-        puts("Gateway Valido");
+        printf("Gateway Valido\n");
+        return TRUE;
     }else{
-        puts("Gateway invalido");
+        printf("Gateway invalido\n");
+        return FALSE;
     }
        
 }
