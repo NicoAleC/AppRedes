d3381bbef766c73f9ca039595b6ce18913ed6953
Nicolas <chincholax@gmail.com>
2018-12-11 19:53:37 -0400

    captura paquetes, reconoce su tamaño y el tipo de protocolo (solo ARP e IP por el momento)

--- sniffer/capturador.c
+++ sniffer/capturador.c
@@ -14,10 +14,11 @@
 int main(int argc, char **argv)
 {
     char errbuff[PCAP_ERRBUF_SIZE];
+    printf("device found\n");
     char *dev = pcap_lookupdev(errbuff);
     
-    char *mascara_de_subred;
-    char *ip;
+    char mascara_de_subred[16];
+    char ip[16];
     bpf_u_int32 mask_raw;
     bpf_u_int32 ip_raw;
     int lookup_code;
@@ -31,11 +32,13 @@ int main(int argc, char **argv)
         fprintf(stderr, "%s\n", errbuff);
         exit(EXIT_FAILURE);
     } else {
-        printf("Dispositivo encontrado:%s",dev);
+        printf("Dispositivo encontrado:%s\n",dev);
     }
 
+    printf("reconociendo las direcciones ip y máscara subred\n");
     lookup_code = pcap_lookupnet(dev, &ip_raw, &mask_raw, errbuff);
-
+    printf("reconocidas las direcciones ip y máscara subred\n");
+    
     if( lookup_code == -1){
         fprintf(stderr, "%s\n", errbuff);
         exit(EXIT_FAILURE);
@@ -49,6 +52,7 @@ int main(int argc, char **argv)
         printf("direccion ip no reconocida\n");
         exit(EXIT_FAILURE);
     }
+    printf("%s\n", ip);
 
     direccion.s_addr = mask_raw;
 
@@ -58,15 +62,17 @@ int main(int argc, char **argv)
         printf("máscara de subred no reconocida\n");
         exit(EXIT_FAILURE);
     }
+    printf("%s\n", mascara_de_subred);
 
+    printf("definiendo handleloop\n");
     handleloop = pcap_open_live(dev, LENGTH_CAPTURE, 1, 10000, errbuff);
 
-    paquete = pcap_next(handleloop, &cabecera_paquete);
+    /*paquete = pcap_next(handleloop, &cabecera_paquete);
 
     if(paquete == NULL){
         printf("error al reconocer un paquete\n");
         exit(EXIT_FAILURE);
-    }
+    }*/
 
     if(handleloop == NULL){
         perror(errbuff);
@@ -74,6 +80,24 @@ int main(int argc, char **argv)
     }
     
     fprintf(stderr, "Empezando a capturar paquetes %s", dev);
-    pcap_loop(handleloop,10,printing,NULL);
+    pcap_loop(handleloop, 0, manejador_paquete, NULL);
     
+}
+
+void informacion_paquete(const u_char *paquete, const struct pcap_pkthdr *cabecera_paquete){
+    fprintf(stderr, "largo del paquete capturado: %d\n", cabecera_paquete->caplen);
+    fprintf(stderr, "largo total del paquete: %d\n", cabecera_paquete->len);
+}
+
+void manejador_paquete(u_char * args, const struct pcap_pkthdr * cabecera_paquete, const u_char * cuerpo_paquete){
+    informacion_paquete(cuerpo_paquete, cabecera_paquete);
+    struct ether_header * cabecera_ethernet;
+
+    cabecera_ethernet = (struct ether_header *) cuerpo_paquete;
+
+    if(ntohs(cabecera_ethernet->ether_type) == ETHERTYPE_IP){
+        printf("IP\n");
+    } else if(ntohs(cabecera_ethernet->ether_type) == ETHERTYPE_ARP) {
+        printf("ARP\n");
+    }
 }
--- sniffer/capturador.h
+++ sniffer/capturador.h
@@ -1,1 +1,3 @@
-void informacion_paquete(const u_char *, struct pcap_pkthdr);
+void informacion_paquete(const u_char *, const struct pcap_pkthdr*);
+
+void manejador_paquete(u_char *, const struct pcap_pkthdr *, const u_char *);
